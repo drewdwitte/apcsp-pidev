e2d6a2cf83251de4368e906f75ea3cb116ff545e
drewdwitte <32139971+drewdwitte@users.noreply.github.com>
2019-06-12 14:26:55 -0700

    redo

---
+++ cdemo/myproject/student.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+void printStudent (struct Student* student)
+{
+  printf("Student First Name: %s\nStudent Last Name: %s\nStudent Age: %d\nStudent ID Number: %d\n", student->firstname, student->lastname,name, student->age, student->id); 
+}
