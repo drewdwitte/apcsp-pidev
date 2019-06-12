1b72d3eead5e379c0e6ebd74e14250615a39108b
drewdwitte <32139971+drewdwitte@users.noreply.github.com>
2019-06-12 15:09:37 -0700

    fixed errors

--- cdemo/myproject/studentmain.c
+++ cdemo/myproject/studentmain.c
@@ -1,6 +1,50 @@
 #include <stdio.h>
 #include "student.h"
 
+int enterStudent (struct Student* student)
+{
+  char first[50], last[50];
+  char studentAge[50], studentId[50];
+  int yearsOld;
+  int studentNumber;
+
+  while (1)
+  {
+    printf("Enter a first name ");
+    fgets(first, 50, stdin);
+    if  (sscanf(first, "%s", student->firstname) == 1) break;
+  }
+
+  while (1)
+  {
+    printf("Enter a last name ");
+    fgets(last, 50, stdin);
+    if  (sscanf(last, "%s", student->lastname) == 1) break;
+      printf("Not an appropriate last name - try again!\n");
+  }
+
+  while (1)
+  {
+    printf("Enter the student's age ");
+    fgets(studentAge, 50, stdin);
+    if  (sscanf(studentAge, "%d", &yearsOld) == 1) break;
+      printf("Not an appropriate age - try again!\n");
+  }
+  student->age = yearsOld;
+
+  while (1)
+  {
+    printf("Enter the student's id number ");
+    fgets(studentId, 50, stdin);
+    if  (sscanf(studentId, "%d", &studentNumber) == 1) break;
+      printf("Not an appropriate id number - try again!\n");
+  } 
+  student->id = studentNumber;
+
+}
+
+
+
 int main() 
 {
   char cont[50];
